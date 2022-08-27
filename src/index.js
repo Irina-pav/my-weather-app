2145bb798b334d8f08c9b62e00e089233cbbdb1f
Irina-pav <irina.pavlova10@gmail.com>
2022-08-27 13:54:07 +0300

    fix js weather forecast day

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class = "row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 3) {
+    if (index < 3 && index > 0) {
       forecastHTML =
         forecastHTML +
         `
